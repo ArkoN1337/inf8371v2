827b2cab51041520ba5d8890eb8c171883308c96
Anass Laaz <54183359+ArkoN1337@users.noreply.github.com>
2023-02-15 07:02:30 -0500

    test

--- moa/src/main/java/moa/classifiers/functions/NoChange.java
+++ moa/src/main/java/moa/classifiers/functions/NoChange.java
@@ -69,6 +69,7 @@ public class NoChange extends AbstractClassifier implements MultiClassClassifier
        
     }
 
+    //comment test
     public boolean isRandomizable() {
         return false;
     }
